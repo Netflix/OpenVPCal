Add in a fix for the avoid_clipping and we move the lut scaling to the calibration

src/open_vp_cal/core/calibrate.py
@@ -69,8 +69,8 @@ def achromatic(rgb, shadow_rolloff):
     return np.asarray([value, value, value])
 
 def eotf_correction_calculation(
-        grey_ramp_screen, grey_signal_values, grey_signal_value_rgb, deltaE_grey_ramp,
-        deltaE_threshold=10):
+        grey_ramp_screen, grey_signal_values, grey_signal_value_rgb, deltaE_grey_ramp, avoid_clipping=True,
+        peak_lum=None, deltaE_threshold=10):
     """ Compute a LUT to correct the EOTF as measured from grey patches. Any grey patches with a delta E greater than
         the deltaE_threshold are ignored.
 
@@ -80,6 +80,9 @@ def eotf_correction_calculation(
         grey_signal_values (array-like): Reference Signal Values For The EOTF
         grey_signal_value_rgb: Reference Signal Values For The EOTF as RGB values
         deltaE_grey_ramp: Delta E Values For The Grey Ramp
+        avoid_clipping: If we want to avoid clipping of values on the led wall we scale any values using the peak lum
+            if values go above this
+        peak_lum: The peak luminance of the led wall
 
     Returns:
         lut_r (array-like): 1D LUT with elements as (y,x)
@@ -107,6 +110,21 @@ def eotf_correction_calculation(
     lut_r = np.array(lut_r)
     lut_g = np.array(lut_g)
     lut_b = np.array(lut_b)
+
+    if avoid_clipping:
+        if not peak_lum:
+            raise ValueError("Peak luminance must be provided if avoid_clipping is True")
+
+        max_r = np.max(np.max(lut_r[:, 0]))
+        max_g = np.max(np.max(lut_g[:, 0]))
+        max_b = np.max(np.max(lut_b[:, 0]))
+        max_value = max(max_r, max_g, max_b)
+        if max_value > peak_lum:
+            scale_factor = peak_lum / max_value
+
+            lut_r *= scale_factor
+            lut_g *= scale_factor
+            lut_b *= scale_factor
 
     return lut_r, lut_g, lut_b
 
@@ -225,10 +243,12 @@ def extract_screen_cs(
         chromatic_adaptation_transform=cs_cat,
     )
 
-    # If we want to avoid clipping we need to scale the matrix so that the sum of each row is 1
-    # if avoid_clipping:
-    #     row_sums = target_to_screen_matrix.sum(axis=1, keepdims=True)
-    #     target_to_screen_matrix = target_to_screen_matrix / row_sums
+    # If we want to avoid clipping, we need to scale the matrix so that the sum of each row is 1
+    if avoid_clipping:
+        row_sums = target_to_screen_matrix.sum(axis=1, keepdims=True)
+        max_value = max(row_sums)
+        if max_value > 1:
+            target_to_screen_matrix = target_to_screen_matrix / max_value
 
     saturated_primaries_input_plate_gamut = colour.RGB_to_RGB(
         saturated_primaries, native_camera_gamut_cs, input_plate_cs, camera_conversion_cat
@@ -843,7 +863,9 @@ def run(
                 eotf_ramp_screen,
                 eotf_signal_values,
                 eotf_signal_value_rgb,
-                delta_e_ICtCP_eotf_ramp
+                avoid_clipping=avoid_clipping,
+                peak_lum=peak_lum,
+                deltaE_threshold=delta_e_ICtCP_eotf_ramp
             )
 
             eotf_ramp_camera_native_gamut_calibrated = ca.vector_dot(
@@ -883,7 +905,9 @@ def run(
             eotf_ramp_camera_native_gamut,
             eotf_signal_values,
             eotf_signal_value_rgb,
-            delta_e_ICtCP_eotf_ramp
+            avoid_clipping=avoid_clipping,
+            peak_lum=peak_lum,
+            deltaE_threshold=delta_e_ICtCP_eotf_ramp
         )
 
         # rgb_ratio is not passed as the LUTs already include this factor

tests/test_calibrate.py
@@ -1,6 +1,6 @@
 import unittest
 import colour
-
+import numpy as np
 
 import open_vp_cal.core.calibrate as calibrate
 
@@ -234,6 +234,52 @@ class TestCalibrate(TestProject):
                                reference_wall_external_white_balance_matrix=None)
 
 
+class TestAvoidClipping(TestProject):
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_avoid_clipping(self):
+        samples = self.get_samples(self.led_wall)
+        reference_samples = self.get_reference_samples(self.led_wall)
+
+        target_colour_space = utils.get_target_colourspace_for_led_wall(self.led_wall)
+        result = calibrate.run(
+            measured_samples=samples, reference_samples=reference_samples,
+            input_plate_gamut=self.led_wall.input_plate_gamut,
+            native_camera_gamut=constants.CameraColourSpace.RED_WIDE_GAMUT,
+            target_gamut=target_colour_space,
+            target_to_screen_cat=self.led_wall.target_to_screen_cat,
+            reference_to_target_cat=self.led_wall.reference_to_target_cat,
+            target_max_lum_nits=self.led_wall.target_max_lum_nits, target_EOTF=None,
+            enable_plate_white_balance=True, enable_gamut_compression=True,
+            enable_EOTF_correction=True, calculation_order=CalculationOrder.CO_CS_EOTF,
+            gamut_compression_shadow_rolloff=constants.GAMUT_COMPRESSION_SHADOW_ROLLOFF,
+            reference_wall_external_white_balance_matrix=None,
+            avoid_clipping=False
+        )
+
+        result2 = calibrate.run(
+            measured_samples=samples, reference_samples=reference_samples,
+            input_plate_gamut=self.led_wall.input_plate_gamut,
+            native_camera_gamut=constants.CameraColourSpace.RED_WIDE_GAMUT,
+            target_gamut=target_colour_space,
+            target_to_screen_cat=self.led_wall.target_to_screen_cat,
+            reference_to_target_cat=self.led_wall.reference_to_target_cat,
+            target_max_lum_nits=self.led_wall.target_max_lum_nits, target_EOTF=None,
+            enable_plate_white_balance=True, enable_gamut_compression=True,
+            enable_EOTF_correction=True, calculation_order=CalculationOrder.CO_CS_EOTF,
+            gamut_compression_shadow_rolloff=constants.GAMUT_COMPRESSION_SHADOW_ROLLOFF,
+            reference_wall_external_white_balance_matrix=None,
+            avoid_clipping=True
+        )
+
+        no_clipping_primaries = result[constants.Results.POST_CALIBRATION_SCREEN_PRIMARIES]
+        no_clipping_whitepoint = result[constants.Results.POST_CALIBRATION_SCREEN_WHITEPOINT]
+
+        clipping_primaries = result2[constants.Results.POST_CALIBRATION_SCREEN_PRIMARIES]
+        clipping_whitepoint = result2[constants.Results.POST_CALIBRATION_SCREEN_WHITEPOINT]
+
+        self.assertTrue(np.allclose(no_clipping_primaries, clipping_primaries, atol=0.00001))
+        self.assertTrue(np.allclose(no_clipping_whitepoint, clipping_whitepoint, atol=0.00001))
+
+
+
+