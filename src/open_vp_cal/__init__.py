7e8a30f379fea9f551c67825053d7bf4b6f51648
Adam Davis <adamdavis@netflix.com>
2024-03-04 13:59:38 +0000

    Upping version for testing

--- src/open_vp_cal/__init__.py
+++ src/open_vp_cal/__init__.py
@@ -1,7 +1,7 @@
 """
 Init Module defines a few module level variables
 """
-__version__ = "1.0.0-rc.12a"
+__version__ = "1.0.0-rc.12b"
 __authors__ = [
     "Adam Davis", "Adrian Pueyo", "Carol Payne", "Francesco Luigi Giardiello"
 ]
