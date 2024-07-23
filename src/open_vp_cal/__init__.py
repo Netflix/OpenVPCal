eaf5e42872430ce470d3e9352b52a52b001588f6
Adam Davis <adamdavis@netflix.com>
2024-07-23 10:51:06 +0100

    up version for next round of development

--- src/open_vp_cal/__init__.py
+++ src/open_vp_cal/__init__.py
@@ -15,7 +15,7 @@ limitations under the License.
 
 Init Module defines a few module level variables
 """
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 __authors__ = [
     "Adam Davis", "Adrian Pueyo", "Carol Payne", "Francesco Luigi Giardiello", "Daniel Heckenberg"
 ]
